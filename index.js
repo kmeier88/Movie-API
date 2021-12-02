cc9ed522a514eb59bc6c06c08f1cd7b40f015dd0
kmeier88 <90733385+kmeier88@users.noreply.github.com>
2021-12-02 17:02:26 -0500

    update documentation

--- index.js
+++ index.js
@@ -50,91 +50,55 @@ app.get('/', (req, res) => {
 //Returns list of all movies
 
 app.get('/movies', (req, res) => {
-  res.json(movies);
+  res.send ('Successful GET request returning data on all movies');
 });
 
 //Returns data about a single movie by title
 
 app.get('/movies/:title', (req, res) => {
-  res.json(movies.find((movie) =>
-    { return movie.title === req.params.title }));
+  res.send ('Successful GET request returning data about a single movie by title');
 });
 
 //Returns data about a genre by movie title
 
 app.get('/movies/:title/:genre', (req, res) => {
-  res.json(movies.find((movie) =>
-    { return movie.genre === req.params.title }));
+  res.send ('Successful GET request returning data about a genre of movie by title');
 });
 
 //Returns data about a director
 
 app.get('/movies/:director', (req, res) => {
-  res.json(movies.find((director) =>
-    { return director.name === req.params.name }));
+  res.send ('Successful GET request returning data about a director');
 });
 
 //Adds new user
 
 app.post('/users', (req, res) => {
-  let newUser = req.body;
-
-  if (!newUser.name) {
-    const message = 'Missing name in request body';
-    res.status(400).send(message);
-  } else {
-    users.push(newUser);
-    res.status(201).send(newUser);
-  }
+  res.send ('Successful POST request allowing a user to register');
 });
 
 //Allows users to update their username
 
 app.put('/users/:username', (req, res) => {
-  let user = users.find((user) => { return user.username === req.params.username });
-
-  if (user) {
-    user.username[req.params.username] = parseInt(req.params.username);
-    res.status(201).send('User was assigned a new username of ' + req.params.username);
-  } else {
-    res.status(404).send('User with the name ' + req.params.username + ' was not found.');
-  }
+  res.send ('Successful PUT request allowing users to update their username');
 });
 
 //Allows users to add movie to list of favorites
 
 app.post('/users/:favorites', (req, res) => {
-  let favMovie = req.body;
-
-  if (!movie.title) {
-    const message = 'Missing name of movie in request body';
-    res.status(400).send(message);
-  } else {
-    users.push(favMovie);
-    res.status(201).send(favMovie);
-  }
+  res.send ('Successfull POST request allowing users to add a movie to list of favorites');
 });
 
 //Allows users to remove movie from list of favorites
 
 app.delete('/users/:favorites', (req, res) => {
-  let user = users.find((user) => { return user.favorites === req.params.favorites });
-
-  if (user) {
-    users = users.filter((obj) => { return obj.favorites !== req.params.favorites});
-    res.status(201).send('Movie ' + req.params.favorites + ' was deleted.');
-  }
+  res.send ('Successful DELETE request allowing users to remove a movie from list of favorites');
 });
 
 //Allow existing user to deregister
 
 app.delete('/users/:username', (req, res) => {
-  let user = users.find((user) => { return user.username === req.params.username });
-
-  if (user) {
-    users = users.filter((obj) => { return obj.username !== req.params.username });
-    res.status(201).send('User ' + req.params.username + ' was deleted.');
-  }
+  res.send ('Sucessful DELETE request allowing users to deregister');
 });
 
 app.use(express.static('public'));
