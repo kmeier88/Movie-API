37a63943136666cd3014bdc722c223f5b8a8ed17
kmeier88 <90733385+kmeier88@users.noreply.github.com>
2022-01-17 10:32:32 -0500

    changed env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Users = Models.User;
 
 //add process.env.connectionURI later
 
-mongoose.connect('process.env.CONNECTION_URI', {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express');
 const morgan = require('morgan');
